f62e371d87c7e53d9c7219e390c91d48f1502207
Raj Singh <99217732+rajasingh310@users.noreply.github.com>
2023-05-23 03:59:30 +0200

    added a new file ConditionalGANS.py, which generates the ToF material images. However, currently the gen loss is increasing and disc loss is going to zero immediately

---
+++ code/ConditionalGANs.py
@@ -0,0 +1,376 @@
+def ConditionalGANs(real_data, real_labels, num_samples, log_write):
+
+    import torch
+    from torch import nn
+    from tqdm.auto import tqdm
+    from torch.utils.data import DataLoader, TensorDataset
+    import torch.nn.functional as F
+    import numpy as np
+    from tensorboardX import SummaryWriter
+
+    def get_generator_block(input_dim, output_dim):
+        '''
+        Function for returning a block of the generator's neural network
+        given input and output dimensions.
+        Parameters:
+            input_dim: the dimension of the input vector, a scalar
+            output_dim: the dimension of the output vector, a scalar
+        Returns:
+            a generator neural network layer, with a linear transformation
+              followed by a batch normalization and then a relu activation
+        '''
+        return nn.Sequential(
+            # Hint: Replace all of the "None" with the appropriate dimensions.
+            # The documentation may be useful if you're less familiar with PyTorch:
+            # https://pytorch.org/docs/stable/nn.html.
+            #### START CODE HERE ####
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            #### END CODE HERE ####
+            nn.ReLU(inplace=True)
+        )
+
+    class Generator(nn.Module):
+        '''
+        Generator Class
+        Values:
+            z_dim: the dimension of the noise vector, a scalar
+            im_dim: the dimension of the images, fitted for the dataset used, a scalar
+              (MNIST images are 28 x 28 = 784 so that is your default)
+            hidden_dim: the inner dimension, a scalar
+        '''
+
+        def __init__(self, input_dim=10, output_dim=784, hidden_dim=32):
+            super(Generator, self).__init__()
+            # Build the neural network
+            self.gen = nn.Sequential(
+                get_generator_block(input_dim, hidden_dim),
+                get_generator_block(hidden_dim, hidden_dim * 2),
+                get_generator_block(hidden_dim * 2, hidden_dim * 4),
+                get_generator_block(hidden_dim * 4, hidden_dim * 8),
+                get_generator_block(hidden_dim * 8, hidden_dim * 16),
+                get_generator_block(hidden_dim * 16, hidden_dim * 32),
+                get_generator_block(hidden_dim * 32, hidden_dim * 64),
+                get_generator_block(hidden_dim * 64, hidden_dim * 128),
+                get_generator_block(hidden_dim * 128, hidden_dim * 64),
+                # There is a dropdown with hints if you need them!
+                #### START CODE HERE ####
+                nn.Linear(hidden_dim * 64, output_dim),
+                nn.Sigmoid()
+                #### END CODE HERE ####
+            )
+
+        def forward(self, noise):
+            '''
+            Function for completing a forward pass of the generator: Given a noise tensor,
+            returns generated images.
+            Parameters:
+                noise: a noise tensor with dimensions (n_samples, z_dim)
+            '''
+            return self.gen(noise)
+
+    def get_noise(n_samples, input_dim, device='cpu'):
+        '''
+        Function for creating noise vectors: Given the dimensions (n_samples, input_dim)
+        creates a tensor of that shape filled with random numbers from the normal distribution.
+        Parameters:
+            n_samples: the number of samples to generate, a scalar
+            input_dim: the dimension of the input vector, a scalar
+            device: the device type
+        '''
+        return torch.randn(n_samples, input_dim, device=device)
+
+    def get_discriminator_block(input_dim, output_dim):
+        '''
+        Discriminator Block
+        Function for returning a neural network of the discriminator given input and output dimensions.
+        Parameters:
+            input_dim: the dimension of the input vector, a scalar
+            output_dim: the dimension of the output vector, a scalar
+        Returns:
+            a discriminator neural network layer, with a linear transformation
+              followed by an nn.LeakyReLU activation with negative slope of 0.2
+              (https://pytorch.org/docs/master/generated/torch.nn.LeakyReLU.html)
+        '''
+        return nn.Sequential(
+            #### START CODE HERE ####
+            nn.Linear(input_dim, output_dim),
+            nn.LeakyReLU(0.2)
+            #### END CODE HERE ####
+        )
+
+    class Discriminator(nn.Module):
+        '''
+        Discriminator Class
+        Values:
+            im_dim: the dimension of the images, fitted for the dataset used, a scalar
+                (MNIST images are 28x28 = 784 so that is your default)
+            hidden_dim: the inner dimension, a scalar
+        '''
+
+        def __init__(self, input_dim=784, output_dim=1, hidden_dim=32):
+            super(Discriminator, self).__init__()
+            self.disc = nn.Sequential(
+                get_discriminator_block(input_dim, hidden_dim * 4),
+                get_discriminator_block(hidden_dim * 4, hidden_dim * 8),
+                get_discriminator_block(hidden_dim * 8, hidden_dim * 16),
+                get_discriminator_block(hidden_dim * 16, hidden_dim * 32),
+                get_discriminator_block(hidden_dim * 32, hidden_dim * 16),
+                get_discriminator_block(hidden_dim * 16, hidden_dim * 8),
+                get_discriminator_block(hidden_dim * 8, hidden_dim * 4),
+                get_discriminator_block(hidden_dim * 4, hidden_dim * 2),
+                get_discriminator_block(hidden_dim * 2, hidden_dim),
+                # Hint: You want to transform the final output into a single value,
+                #       so add one more linear map.
+                #### START CODE HERE ####
+                nn.Linear(hidden_dim, output_dim)
+                #### END CODE HERE ####
+            )
+
+        def forward(self, image):
+            '''
+            Function for completing a forward pass of the discriminator: Given an image tensor,
+            returns a 1-dimension tensor representing fake/real.
+            Parameters:
+                image: a flattened image tensor with dimension (im_dim)
+            '''
+            return self.disc(image)
+
+    def get_one_hot_labels(labels, n_classes):
+        '''
+        Function for creating one-hot vectors for the labels, returns a tensor of shape (?, num_classes).
+        Parameters:
+            labels: tensor of labels from the dataloader, size (?)
+            n_classes: the total number of classes in the dataset, an integer scalar
+        '''
+
+        return F.one_hot(labels, n_classes)
+
+    assert (
+            get_one_hot_labels(
+                labels=torch.Tensor([[0, 2, 1]]).long(),
+                n_classes=3
+            ).tolist() ==
+            [[
+                [1, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0]
+            ]]
+    )
+
+
+    # Check that the device of get_one_hot_labels matches the input device
+    if torch.cuda.is_available():
+        assert str(get_one_hot_labels(torch.Tensor([[0]]).long().cuda(), 1).device).startswith("cuda")
+
+    print("Success!")
+
+    def combine_vectors(x, y):
+        '''
+        Function for combining two vectors with shapes (n_samples, ?) and (n_samples, ?).
+        Parameters:
+          x: (n_samples, ?) the first vector.
+            In this assignment, this will be the noise vector of shape (n_samples, z_dim),
+            but you shouldn't need to know the second dimension's size.
+          y: (n_samples, ?) the second vector.
+            Once again, in this assignment this will be the one-hot class vector
+            with the shape (n_samples, n_classes), but you shouldn't assume this in your code.
+        '''
+        # Note: Make sure this function outputs a float no matter what inputs it receives
+
+        combined = torch.cat((x.float(), y.float()), dim=1)
+        return combined
+
+    combined = combine_vectors(torch.tensor([[1, 2], [3, 4]]), torch.tensor([[5, 6], [7, 8]]))
+    if torch.cuda.is_available():
+        # Check that it doesn't break with cuda
+        cuda_check = combine_vectors(torch.tensor([[1, 2], [3, 4]]).cuda(), torch.tensor([[5, 6], [7, 8]]).cuda())
+        assert str(cuda_check.device).startswith("cuda")
+    # Check exact order of elements
+    assert torch.all(combined == torch.tensor([[1, 2, 5, 6], [3, 4, 7, 8]]))
+    # Tests that items are of float type
+    assert (type(combined[0][0].item()) == float)
+    # Check shapes
+    combined = combine_vectors(torch.randn(1, 4, 5), torch.randn(1, 8, 5))
+    assert tuple(combined.shape) == (1, 12, 5)
+    assert tuple(combine_vectors(torch.randn(1, 10, 12).long(), torch.randn(1, 20, 12).long()).shape) == (1, 30, 12)
+    # Check that the float transformation doesn't happen after the inputs are concatenated
+    assert tuple(combine_vectors(torch.randn(1, 10, 12).long(), torch.randn(1, 20, 12)).shape) == (1, 30, 12)
+    print("Success!")
+
+    # ## Training
+    # Now you can start to put it all together!
+    # First, you will define some new parameters:
+    #
+    # *   n_classes: the number of classes in MNIST (10, since there are the digits from 0 to 9)
+
+    # In[ ]:
+
+    real_data_shape = (real_data.shape[1], real_data.shape[2], real_data.shape[3], real_data.shape[4])
+    n_classes = len(np.unique(real_labels))
+
+    # And you also include the same parameters from previous assignments:
+    #
+    #   *   criterion: the loss function
+    #   *   n_epochs: the number of times you iterate through the entire dataset when training
+    #   *   z_dim: the dimension of the noise vector
+    #   *   display_step: how often to display/visualize the images
+    #   *   batch_size: the number of images per forward/backward pass
+    #   *   lr: the learning rate
+    #   *   device: the device type
+    #
+
+    # In[ ]:
+
+    criterion = nn.BCEWithLogitsLoss()
+    n_epochs = 30
+    z_dim = 64
+    batch_size = 1000
+    lr = 0.002
+    device = 'cuda'
+
+    real_data = np.reshape(real_data, (real_data.shape[0], 144))
+    real_dataset = TensorDataset(torch.Tensor(real_data).to(device), torch.Tensor(real_labels).to(device))
+    dataloader = DataLoader(real_dataset, batch_size=batch_size, shuffle=True)
+
+    # Then, you can initialize your generator, discriminator, and optimizers. To do this, you will need to update the input dimensions for both models. For the generator, you will need to calculate the size of the input vector; recall that for conditional GANs, the generator's input is the noise vector concatenated with the class vector. For the discriminator, you need to add a channel for every class.
+
+    # In[ ]:
+
+    # UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+    # GRADED FUNCTION: get_input_dimensions
+    def get_input_dimensions(z_dim, real_data_shape, n_classes):
+        '''
+        Function for getting the size of the conditional input dimensions
+        from z_dim, the image shape, and number of classes.
+        Parameters:
+            z_dim: the dimension of the noise vector, a scalar
+            mnist_shape: the shape of each MNIST image as (C, W, H), which is (1, 28, 28)
+            n_classes: the total number of classes in the dataset, an integer scalar
+                    (10 for MNIST)
+        Returns:
+            generator_input_dim: the input dimensionality of the conditional generator,
+                              which takes the noise and class vectors
+            discriminator_im_chan: the number of input channels to the discriminator
+                                (e.g. C x 28 x 28 for MNIST)
+        '''
+        generator_input_dim = z_dim + n_classes
+        discriminator_im_chan = real_data_shape[0]*real_data_shape[1]*real_data_shape[2]*real_data_shape[3] + n_classes
+        return generator_input_dim, discriminator_im_chan
+
+    generator_input_dim, discriminator_im_chan = get_input_dimensions(z_dim, real_data_shape, n_classes)
+
+    gen = Generator(input_dim=generator_input_dim, output_dim=discriminator_im_chan-n_classes).to(device)
+    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+    disc = Discriminator(input_dim=discriminator_im_chan).to(device)
+    disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+    def weights_init(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        if isinstance(m, nn.BatchNorm2d):
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+            torch.nn.init.constant_(m.bias, 0)
+
+    gen = gen.apply(weights_init)
+    disc = disc.apply(weights_init)
+
+    # Now to train, you would like both your generator and your discriminator to know what class of image should be generated. There are a few locations where you will need to implement code.
+    #
+    # For example, if you're generating a picture of the number "1", you would need to:
+    #
+    # 1.   Tell that to the generator, so that it knows it should be generating a "1"
+    # 2.   Tell that to the discriminator, so that it knows it should be looking at a "1". If the discriminator is told it should be looking at a 1 but sees something that's clearly an 8, it can guess that it's probably fake
+    #
+    discriminator_losses = []
+
+    if log_write:
+        writer = SummaryWriter()
+
+    for epoch in range(n_epochs):
+        # Dataloader returns the batches and the labels
+        for real, labels in tqdm(dataloader):
+            cur_batch_size = len(real)
+            # Flatten the batch of real images from the dataset
+            real = real.to(device)
+            labels = torch.squeeze(labels.to(device)).long()
+            one_hot_labels = get_one_hot_labels(labels, n_classes)
+
+            ### Update discriminator ###
+            # Zero out the discriminator gradients
+            disc_opt.zero_grad()
+            # Get noise corresponding to the current batch_size
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+
+            # Now you can get the images from the generator
+            # Steps: 1) Combine the noise vectors and the one-hot labels for the generator
+            #        2) Generate the conditioned fake images
+
+            noise_and_labels = combine_vectors(fake_noise, one_hot_labels)
+            fake = gen(noise_and_labels)
+
+            # Make sure that enough images were generated
+            assert len(fake) == len(real)
+            # Check that correct tensors were combined
+            assert tuple(noise_and_labels.shape) == (cur_batch_size, fake_noise.shape[1] + one_hot_labels.shape[1])
+            # It comes from the correct generator
+            assert tuple(fake.shape) == (len(real), discriminator_im_chan-n_classes)
+
+            # Now you can get the predictions from the discriminator
+            # Steps: 1) Create the input for the discriminator
+            #           a) Combine the fake images with image_one_hot_labels,
+            #              remember to detach the generator (.detach()) so you do not backpropagate through it
+            #           b) Combine the real images with image_one_hot_labels
+            #        2) Get the discriminator's prediction on the fakes as disc_fake_pred
+            #        3) Get the discriminator's prediction on the reals as disc_real_pred
+
+            #### START CODE HERE ####
+            fake_image_and_labels = combine_vectors(fake, one_hot_labels)
+            real_image_and_labels = combine_vectors(real, one_hot_labels)
+            disc_fake_pred = disc(fake_image_and_labels.detach())
+            disc_real_pred = disc(real_image_and_labels)
+            #### END CODE HERE ####
+
+            disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+            disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+            disc_loss = (disc_fake_loss + disc_real_loss) / 2
+            disc_loss.backward(retain_graph=True)
+            disc_opt.step()
+
+            # Keep track of the average discriminator loss
+            discriminator_losses += [disc_loss.item()]
+
+            ### Update generator ###
+            # Zero out the generator gradients
+            gen_opt.zero_grad()
+
+            fake_image_and_labels = combine_vectors(fake, one_hot_labels)
+            # This will error if you didn't concatenate your labels to your image correctly
+            disc_fake_pred = disc(fake_image_and_labels)
+            gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+            gen_loss.backward()
+            gen_opt.step()
+
+            if log_write:
+                writer.add_scalar('Loss/Generator', gen_loss, epoch)
+                writer.add_scalar('Loss/Discriminator', disc_loss, epoch)
+
+        print("Epoch: {}/50, generator Loss: {:.4f}, Discriminator Loss: {:.4f}".format(
+            epoch + 1,
+            gen_loss,
+            disc_loss))
+
+    # ## Exploration
+    # You can do a bit of exploration now!
+
+    # In[ ]:
+
+    # Before you explore, you should put the generator
+    # in eval mode, both in general and so that batch norm
+    # doesn't cause you issues and is using its eval statistics
+    gen = gen.eval()
+
+    # #### Changing the Class Vector
+    # You can generate some numbers with your new model! You can add interpolation as well to make it more interesting.
+
+    return 2, 4
--- code/main.py
+++ code/main.py
@@ -10,82 +10,96 @@ from images_labels import images_and_labels
 import numpy as np
 import math
 from deep_neural_networks import algorithm_fcnn
+from ConditionalGANs import ConditionalGANs
 
-log_write = False
+log_write = True
+preload_data = True
 
-'''
-    Directory name for dataset:
-    dataset_2 = Fourteen material (14M) dataset
-    dataset_3 = Five material dataset acquired by Mr. Santosh Kasam in the year 2021
-    
-'''
+if preload_data == False:
+    '''
+        Directory name for dataset:
+        dataset_1 = Fifteen material (15M) dataset or 12M
+        dataset_2 = Fourteen material (14M) dataset
+        dataset_3 = Five material dataset acquired by Mr. Santosh Kasam in the year 2021
 
-# Directory path
-dataset_dir = 'dataset_3'
+    '''
 
-full_dataset_dir = os.path.join('..\\datasets', dataset_dir)
-print(full_dataset_dir)
+    # Directory path
+    dataset_dir = 'dataset_2'
 
-# Find .mat file in the directory
-mat_files = [f for f in os.listdir(full_dataset_dir) if f.endswith('.mat')]
+    full_dataset_dir = os.path.join('..\\datasets', dataset_dir)
+    print(full_dataset_dir)
 
-if len(mat_files) == 0:
-    print('No .mat files found in directory')
-else:
-    # Load the first .mat file found
-    file_path = os.path.join(full_dataset_dir, mat_files[0])
-    data = scipy.io.loadmat(file_path)
+    # Find .mat file in the directory
+    mat_files = [f for f in os.listdir(full_dataset_dir) if f.endswith('.mat')]
 
-# List all the variable names present in the .mat file
-print(data.keys())
+    if len(mat_files) == 0:
+        print('No .mat files found in directory')
+    else:
+        # Load the first .mat file found
+        file_path = os.path.join(full_dataset_dir, mat_files[0])
+        data = scipy.io.loadmat(file_path)
 
-dataset = data['dataset']
-amp_imgs = data['amp_imgs']
-depth_imgs = data['depth_imgs']
-f_0 = data['f_0']
-c = data['c']
+    # List all the variable names present in the .mat file
+    print(data.keys())
 
+    dataset = data['dataset']
+    amp_imgs = data['amp_imgs']
+    depth_imgs = data['depth_imgs']
+    f_0 = data['f_0']
+    c = data['c']
 
-if dataset_dir == "dataset_3":
-    mat_num = 5
-    inst_num = 70
-    pix_range_cols = 'C:L'
-    height_window = 60
-    width_window = 80
-elif dataset_dir == 'dataset_2':
-    mat_num = 14
-    inst_num = 3
-    pix_range_cols = 'C:AD'
-    height_window = 60
-    width_window = 80
+    if dataset_dir == 'dataset_1':
+        pass
 
-# Find .xlsx file in the directory
-xlsx_files = [f for f in os.listdir(full_dataset_dir) if f.endswith('.xlsx')]
+    elif dataset_dir == 'dataset_2':
+        mat_num = 14
+        inst_num = 3
+        pix_range_cols = 'C:AD'
+        height_window = 60
+        width_window = 80
+    elif dataset_dir == "dataset_3":
+        mat_num = 5
+        inst_num = 70
+        pix_range_cols = 'C:L'
+        height_window = 60
+        width_window = 80
 
-if len(xlsx_files) == 0:
-    print('No .xlsx files found in directory')
-else:
-    # Load the first .xlsx file found
-    file_path = os.path.join(full_dataset_dir, xlsx_files[0])
-    mat_list_names = pd.read_excel(file_path, header=None, skiprows=1, nrows=mat_num, usecols='A')
-    mat_list_names = mat_list_names.values
-    print(mat_list_names)
-    # Load the second .xlsx file found
-    file_path = os.path.join(full_dataset_dir, xlsx_files[1])
-    pix_ranges = pd.read_excel(file_path, header=None, skiprows=2, nrows=2*inst_num, usecols=pix_range_cols)
-    pix_ranges = pix_ranges.values
+    # Find .xlsx file in the directory
+    xlsx_files = [f for f in os.listdir(full_dataset_dir) if f.endswith('.xlsx')]
 
-# Call the normalization function with the appropriate arguments
-new_dataset = normalization(dataset, amp_imgs, depth_imgs, f_0, c, pix_ranges)
-train_val_test = downsample_dataset(new_dataset, height_window, width_window)    # balancing the dataset
-sigma = find_sigma1(train_val_test)
+    if len(xlsx_files) == 0:
+        print('No .xlsx files found in directory')
+    else:
+        # Load the first .xlsx file found
+        file_path = os.path.join(full_dataset_dir, xlsx_files[0])
+        mat_list_names = pd.read_excel(file_path, header=None, skiprows=1, nrows=mat_num, usecols='A')
+        mat_list_names = mat_list_names.values
+        print(mat_list_names)
+        # Load the second .xlsx file found
+        file_path = os.path.join(full_dataset_dir, xlsx_files[1])
+        pix_ranges = pd.read_excel(file_path, header=None, skiprows=2, nrows=2 * inst_num, usecols=pix_range_cols)
+        pix_ranges = pix_ranges.values
 
-window_rows = 5
-window_cols = 5
-window_stride = 1
-k_nearest = 9
+    # Call the normalization function with the appropriate arguments
+    new_dataset = normalization(dataset, amp_imgs, depth_imgs, f_0, c, pix_ranges)
+    train_val_test = downsample_dataset(new_dataset, height_window, width_window)  # balancing the dataset
+    sigma = find_sigma1(train_val_test)
 
-[x_data, y_data] = images_and_labels(train_val_test, window_rows, window_cols, window_stride, mat_list_names, k_nearest, sigma)
+    window_rows = 5
+    window_cols = 5
+    window_stride = 1
+    k_nearest = 9
+
+    [x_data, y_data] = images_and_labels(train_val_test, window_rows, window_cols, window_stride, mat_list_names, k_nearest, sigma)
+
+    # Save the data
+    np.savez('data.npz', x=x_data, y=y_data)
+
+# Load the data
+loaded_data = np.load('data.npz')
+x_data = loaded_data['x']
+y_data = loaded_data['y']
 
 
 
@@ -109,10 +123,12 @@ x_val = shuffled_x_data[math.floor(train_val_ratio*dim_size+1):, :, :, :, :]
 y_val = shuffled_y_data[math.floor(train_val_ratio*dim_size+1):, ]
 
 # Execute algorithm_fcnn function
-net = algorithm_fcnn(x_train, x_val, y_train, y_val, log_write)
+#net = algorithm_fcnn(x_train, x_val, y_train, y_val, log_write)
 
 # Print final result
 print("Training completed!")
+num_samples = 200
+generated_data, generated_data_labels = ConditionalGANs(x_data, y_data, num_samples, log_write)
 
 
 
